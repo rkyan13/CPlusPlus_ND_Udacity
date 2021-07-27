b35834a5cae349dd04ba4a60e9517e07ac68779f
kswamin4 <rkyan13@github.com>
2021-07-27 16:58:08 -0400

    P3: added Part3-OOP : the udacity started folder for Part3

---
+++ Part3-OOP/P2-L3-Advanced_OOP/P2-L3-C14-Multiple_inheritance/Inheritance_Lab3.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+class Animal {
+public:
+    double age;
+};
+
+class Pet {
+public:
+    std::string name;
+};
+
+// Dog derives from *both* Animal and Pet
+class Dog : public Animal, public Pet {
+public:
+    std::string breed;
+};
+
+int main()
+{
+    assert(cat.color == "black");
+    assert(cat.age == 10);
+    assert(cat.name == "Max");
+}
---
+++ Part3-OOP/P2-L3-Advanced_OOP/P2-L3-C17-Templates/Templates.cpp
@@ -0,0 +1,8 @@
+
+#include <assert.h>
+
+// TODO: Create a generic function Product that multiplies two parameters
+
+int main() { 
+  assert(Product<int>(10, 2) == 20); 
+}
