25641dad3f551871e5febb503c14dd2371705cc4
kswamin4 <rkyan13@github.com>
2021-12-07 16:24:17 -0500

    renamed folders and files in Part3-OOP/P2-L2-Intro_OOP to Part3-OOP/P3-L2-Intro_OOP

---
+++ Part3-OOP/P3-L2-Intro_OOP/P2-L2-C13-Constant_member/InitializationList_Lab2.cpp
@@ -0,0 +1,19 @@
+
+#include <assert.h>
+#include <string>
+
+struct Person {
+public:
+  // TODO: Add an initialization list
+  Person(std::string name) {
+      Person::name = name;
+  }
+  std::string const name;
+};
+
+// Test
+int main() {
+  Person alice("Alice");
+  Person bob("Bob");
+  assert(alice.name != bob.name);
+}
