58919179588a4576ea6f42d073a62cdff3f6cb34
kswamin4 <rkyan13@github.com>
2021-12-26 17:09:13 -0500

    added Part2-Foundations/P2-L2-Intro_to_CPP/P2-L2-C8-Primitive_Variable_Types/

---
+++ Part2-Foundations/P2-L2-Intro_to_CPP/P2-L2-C8-Primitive_Variable_Types/primitive_variable_type_code1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <string>
+using std::cout;
+
+int main() {
+    // Declaring and initializing an int variable.
+    int a = 9;
+    
+    // Declaring a string variable without initializing immediately.
+    std::string b;
+    
+    // Initializing the string b in the next step.
+    b = "Here is a string";
+    
+    cout << a << "\n";
+    cout << b << "\n";
+}
+
+
+
---
+++ Part2-Foundations/P2-L2-Intro_to_CPP/P2-L2-C8-Primitive_Variable_Types/primitive_variable_type_code2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declare and initialize j here.
+    int j= 10;
+    cout<<"j="<<j<<endl;
+}
+
